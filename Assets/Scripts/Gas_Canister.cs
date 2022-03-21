759bf9a5d68c2ec90263414903de125802e4311e
Max Swietnicki <swietnickimax@gmail.com>
2022-03-21 17:33:23 -0400

    Made canister a collectible

--- Assets/Scripts/Gas_Canister.cs
+++ Assets/Scripts/Gas_Canister.cs
@@ -1,32 +1,49 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Collectibles;
 using Controllers;
 using UnityEngine;
 
-public class Gas_Canister : MonoBehaviour
+public class Gas_Canister : Base_Collectible_Behavior
 {
     public Controller_Fuel controllerFuel;
     // Start is called before the first frame update
-    void Start()
+    private bool collected = false;
+    // Update is called once per frame
+
+    public override void MovementUpdate()
+    {
+        gameObject.transform.position -= new Vector3(0,0,0.01f);
+        if (transform.position.z <= -11)
+        {
+            Destroy(this.gameObject);
+        }    
+    }
+
+    public override void SetupCollectible()
     {
         controllerFuel = GameObject.Find("Controller").GetComponent<Controller_Fuel>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override bool CollectionCondition()
     {
-       gameObject.transform.position -= new Vector3(0,0,0.01f);
-       if (transform.position.z <= -11)
-       {
-           Destroy(this.gameObject);
-       }
+        return collected;
+    }
+
+    public override void UpdateVisuals()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Collect()
+    {
+        controllerFuel.SetFuel(controllerFuel.currentFuel+5);
     }
 
     private void OnCollisionEnter(Collision other)
     {
         if (!other.gameObject.CompareTag("Player")) return;
-        controllerFuel.SetFuel(controllerFuel.currentFuel+5);
-        Destroy(this.gameObject);
+        collected = true;
     }
 }
